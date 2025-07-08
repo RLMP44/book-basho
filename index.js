6d383fdce61d99d1ca827bcdf568f18fccbc44c3
Rachael Momber <rlmomber@gmail.com>
2025-07-08 16:59:48 -0400

    add: create tables and seeds, connect frontend

--- index.js
+++ index.js
@@ -23,9 +23,40 @@ db.connect();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-app.get("/", (req, res) => {
- res.render("index.ejs");
+const currentUserId = 1;
+
+async function getBooks() {
+  const query = `
+    SELECT
+      rb.rating,
+      rb.date_started,
+      rb.date_finished,
+      rb.note,
+      rb.summary,
+      book.title AS book_title,
+      book.cover AS book_cover,
+      book.author AS book_author,
+      book.isbn AS book_isbn,
+      users.name AS user_name
+    FROM read_book rb
+    JOIN book ON rb.book_id = book.id
+    JOIN users ON rb.user_id = users.id
+    WHERE user_id = $1;
+  `
+  const results = await db.query(query, [currentUserId]);
+  return results.rows[0];
+}
+
+app.get("/", async (req, res) => {
+  try {
+    const data = await getBooks();
+    res.render("index.ejs", { data: data });
+  } catch (error) {
+    console.log(error);
+  }
 });
+
+// TODO: add notes/:id show page
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
