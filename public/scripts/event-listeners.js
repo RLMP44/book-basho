83aecdfe58f5177dcb76a6a3df148679e41f8b8e
Rachael Momber <rlmomber@gmail.com>
2025-10-04 13:40:45 -0400

    refactor: consolidate event listeners and make reusable

--- public/scripts/event-listeners.js
+++ public/scripts/event-listeners.js
@@ -1,13 +1,9 @@
 import { searchFunction, sendPostRequest } from './functions.js';
 
-const usernameEditTrigger = document.querySelector(".username-edit-trigger");
-const usernameEditField = document.querySelector(".username-edit-toggle");
-const bioEditTrigger = document.querySelector(".bio-edit-trigger");
-const bioEditField = document.querySelector(".bio-edit-toggle");
-const nameEditForm = document.getElementById("name-edit-form");
-const bioEditForm = document.getElementById("bio-edit-form");
 const privateCheckboxes = document.querySelectorAll('input[name="private"]');
 const searchButton = document.getElementById("search-button");
+const triggers = document.querySelectorAll(".edit-trigger");
+const forms = document.querySelectorAll(".edit-toggle");
 
 // ---------- Buttons ----------
 searchButton.addEventListener('click', searchFunction);
@@ -15,36 +11,28 @@ searchButton.addEventListener('click', searchFunction);
 
 // ---------- Triggers ----------
 // display edit form and hide edit button
-usernameEditTrigger.addEventListener("click", () => {
-  usernameEditTrigger.classList.add("hidden");
-  usernameEditField.classList.remove("hidden");
-});
-
-bioEditTrigger.addEventListener("click", () => {
-  bioEditTrigger.classList.add("hidden");
-  bioEditField.classList.remove("hidden");
-});
+triggers.forEach((trigger) => {
+  trigger.addEventListener("click", (event) => {
+    trigger.classList.add("hidden");
+    const form = event.currentTarget.parentNode.nextElementSibling
+    form.classList.remove("hidden");
+  });
+})
 
 
 // ---------- Forms ----------
 // hide form and submit contents on enter, also update UI
-nameEditForm.addEventListener('keydown', function (event) {
-  if (event.key === "Enter") {
-    event.preventDefault();
-    sendPostRequest(this);
-    usernameEditTrigger.classList.remove("hidden");
-    usernameEditField.classList.add("hidden");
-  }
-});
-
-bioEditForm.addEventListener('keydown', function (event) {
-  if (event.key === "Enter") {
-    event.preventDefault();
-    sendPostRequest(this);
-    bioEditTrigger.classList.remove("hidden");
-    bioEditField.classList.add("hidden");
-  }
-});
+forms.forEach((form) => {
+  form.addEventListener('keydown', function (event) {
+    if (event.key === "Enter") {
+      event.preventDefault();
+      const trigger = event.currentTarget.previousElementSibling.querySelector(".edit-trigger");
+      sendPostRequest(this);
+      trigger.classList.remove("hidden");
+      form.classList.add("hidden");
+    }
+  });
+})
 
 
 // ---------- Checkboxes ----------
